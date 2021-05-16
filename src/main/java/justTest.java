41300d03841edaf91f0cfd190c4ea031bc360398
kaplanst <kaplanst@mail.ru>
2021-05-16 18:00:18 -0400

    Utils added

--- src/main/java/justTest.java
+++ src/main/java/justTest.java
@@ -1,37 +1,14 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class justTest {
+public class justTest extends Utils {
 
-    @BeforeClass
-    public void before(){
-        WebDriverManager.chromedriver().setup();
-    }
-    private WebDriver driver;
-
-    @BeforeMethod
-    public void beforeTest(){
-        driver = new ChromeDriver();
-    }
     @Test
-//The end
-        public void blogLinkTest() {
+    public void blogLinkTest() {
         driver.get("http://exler.ru");
-        driver.manage().window().maximize();
         driver.findElement(By.xpath("//div[@class='custom-section-title']/a[@href='/blog/']")).click();
         String result = driver.getTitle();
         Assert.assertEquals(result, "Блог");
-    }
-
-    @AfterMethod
-    public void afterTest(){
-        driver.quit();
     }
 }
