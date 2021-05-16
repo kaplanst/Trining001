8b5a99f966acad0519e1c54d4fbc80a9e6f1945f
kaplanst <kaplanst@mail.ru>
2021-05-16 18:46:16 -0400

    2 tests added

--- src/main/java/justTest.java
+++ src/main/java/justTest.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -6,9 +7,24 @@ public class justTest extends Utils {
 
     @Test
     public void blogLinkTest() {
-        driver.get("http://exler.ru");
-        driver.findElement(By.xpath("//div[@class='custom-section-title']/a[@href='/blog/']")).click();
+        driver.get("https://exler.ru");
+        driver.findElement(By.xpath("//div[@class='menu']//li[2]")).click();
         String result = driver.getTitle();
         Assert.assertEquals(result, "Блог");
+    }
+    @Test
+    public void kinoLinkTest() {
+        driver.get("https://exler.ru");
+        driver.findElement(By.xpath("//div[@class='menu']//li[3]")).click();
+        String result = driver.getTitle();
+        Assert.assertEquals(result, "Кинорецензии");
+    }
+    @Test
+    public void logoTest() {
+        boolean res;
+        driver.get("https://www.exler.ru/");
+        if (driver.findElements(By.xpath("//div[@class = 'header-top']")).size() > 0) res = true;
+        else res = false;
+        Assert.assertTrue(res);
     }
 }
